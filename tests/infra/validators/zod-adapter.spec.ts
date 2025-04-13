bc9de5855972cf06709c58dcc5c0e3567198359b
Lucas Vieira <lucas_99silva@hotmail.com>
2025-04-13 12:53:57 -0300

    test: ensure ZodAdapter calls z.email with correct message

--- tests/infra/validators/zod-adapter.spec.ts
+++ tests/infra/validators/zod-adapter.spec.ts
@@ -1,11 +1,18 @@
-import { z, ZodRawShape, ZodTypeAny } from 'zod'
+import { z, ZodRawShape, ZodString } from 'zod'
 
-jest.mock('zod', () => ({
-  z: {
-    object: jest.fn(),
-    string: jest.fn(),
-  },
-}))
+jest.mock('zod', () => {
+  const emailMock = jest.fn()
+  const stringMock = jest.fn(() => ({
+    email: emailMock,
+  }))
+
+  return {
+    z: {
+      object: jest.fn(),
+      string: stringMock,
+    },
+  }
+})
 
 class ZodAdapter {
   private fieldName: string
@@ -21,7 +28,13 @@ class ZodAdapter {
   }
 
   string(message: string): ZodAdapter {
-    this.validations[this.fieldName] = z.string({ message }) as ZodTypeAny
+    this.validations[this.fieldName] = z.string({ message }) as ZodString
+    return this
+  }
+
+  email(message: string): ZodAdapter {
+    const current = this.validations[this.fieldName] as ZodString
+    this.validations[this.fieldName] = current.email({ message })
     return this
   }
 
@@ -38,6 +51,20 @@ describe('ZodAdapter', () => {
 
     expect(z.string).toHaveBeenCalledWith({
       message: 'any_message_error',
+    })
+  })
+
+  it('should call z.email with correct message', () => {
+    const sut = ZodAdapter
+
+    sut
+      .field('any_field')
+      .string('any_message_error')
+      .email('any_email_error')
+      .build()
+
+    expect(z.string().email).toHaveBeenCalledWith({
+      message: 'any_email_error',
     })
   })
 })
