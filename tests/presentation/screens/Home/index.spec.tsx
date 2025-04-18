ab883ba7290233b7b6013f2936c519300035d600
Lucas Vieira <lucas_99silva@hotmail.com>
2025-04-18 20:47:55 -0300

    test: ensure Home shows emailError if field email is not an email

--- tests/presentation/screens/Home/index.spec.tsx
+++ tests/presentation/screens/Home/index.spec.tsx
@@ -114,4 +114,25 @@ describe('<Home />', () => {
       expect(errorEmail.props.children).toBe('O e-mail é obrigatório')
     })
   })
+
+  it('should show emailError if field email is not an email', async () => {
+    const addAccountMock = new AddAccountMock()
+    render(
+      <GluestackUIProvider>
+        <Home addAccount={addAccountMock} />
+      </GluestackUIProvider>,
+    )
+
+    const openAccountButton = screen.getByTestId('open-account-button')
+    fireEvent(openAccountButton, 'press')
+    const inputEmail = await screen.findByTestId('input-email')
+    fireEvent(inputEmail, 'changeText', 'any_invalid_email')
+    const submitButton = await screen.findByTestId('submit-button')
+    fireEvent(submitButton, 'press')
+    const errorEmail = await screen.findByTestId('error-email')
+
+    await waitFor(async () => {
+      expect(errorEmail.props.children).toBe('Endereço de e-mail inválido')
+    })
+  })
 })
