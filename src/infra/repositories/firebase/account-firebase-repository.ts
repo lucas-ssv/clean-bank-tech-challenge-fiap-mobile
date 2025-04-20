e359b20f0daf08f26c0822d768d65cf770b0042f
Lucas Vieira <lucas_99silva@hotmail.com>
2025-04-20 17:40:56 -0300

    feat: ensure AccountFirebaseRepository authenticates on success

--- src/infra/repositories/firebase/account-firebase-repository.ts
+++ src/infra/repositories/firebase/account-firebase-repository.ts
@@ -1,18 +1,27 @@
-import { createUserWithEmailAndPassword } from 'firebase/auth'
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from 'firebase/auth'
 import { addDoc, collection, Timestamp } from 'firebase/firestore'
 
 import { auth, db } from '@/main/config/firebase'
 import {
   AddAccountRepository,
   AddAccountRepositoryParams,
+  LoadAccountRepository,
+  LoadAccountRepositoryParams,
   SaveUserRepository,
   SaveUserRepositoryParams,
 } from '@/data/contracts'
 import { userConverter } from './converters'
 
 export class AccountFirebaseRepository
-  implements AddAccountRepository, SaveUserRepository
+  implements LoadAccountRepository, AddAccountRepository, SaveUserRepository
 {
+  async auth(user: LoadAccountRepositoryParams): Promise<void> {
+    await signInWithEmailAndPassword(auth, user.email, user.password)
+  }
+
   async add(account: AddAccountRepositoryParams): Promise<void> {
     await createUserWithEmailAndPassword(auth, account.email, account.password)
   }
