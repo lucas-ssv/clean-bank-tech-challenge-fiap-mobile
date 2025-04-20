35a9f1e2219d8587a518db67674a996cb876fadf
Lucas Vieira <lucas_99silva@hotmail.com>
2025-04-19 22:23:05 -0300

    refactor: move add method to a new describe

--- tests/infra/repositories/firebase/firebase-adapter.spec.ts
+++ tests/infra/repositories/firebase/firebase-adapter.spec.ts
@@ -14,36 +14,38 @@ jest.mock('firebase/app', () => ({
 }))
 
 describe('FirebaseAdapter', () => {
-  it('should add an account on success', async () => {
-    const sut = new FirebaseAdapter()
+  describe('add()', () => {
+    it('should add an account on success', async () => {
+      const sut = new FirebaseAdapter()
 
-    await sut.add({
-      name: 'any_name',
-      email: 'any_email@mail.com',
-      password: 'any_password',
+      await sut.add({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+      })
+
+      expect(createUserWithEmailAndPassword).toHaveBeenCalledWith(
+        auth,
+        'any_email@mail.com',
+        'any_password',
+      )
     })
 
-    expect(createUserWithEmailAndPassword).toHaveBeenCalledWith(
-      auth,
-      'any_email@mail.com',
-      'any_password',
-    )
-  })
+    it('should throw if createUserWithEmailAndPassword throws', async () => {
+      const sut = new FirebaseAdapter()
+      ;(createUserWithEmailAndPassword as jest.Mock).mockImplementationOnce(
+        () => {
+          throw new Error()
+        },
+      )
 
-  it('should throw if createUserWithEmailAndPassword throws', async () => {
-    const sut = new FirebaseAdapter()
-    ;(createUserWithEmailAndPassword as jest.Mock).mockImplementationOnce(
-      () => {
-        throw new Error()
-      },
-    )
+      const promise = sut.add({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+      })
 
-    const promise = sut.add({
-      name: 'any_name',
-      email: 'any_email@mail.com',
-      password: 'any_password',
+      await expect(promise).rejects.toThrow()
     })
-
-    await expect(promise).rejects.toThrow()
   })
 })
