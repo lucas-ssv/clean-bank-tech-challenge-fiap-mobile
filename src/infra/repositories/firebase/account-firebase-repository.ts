526c249eb15da46ed647864a29ca14b375034aaa
Lucas Vieira <lucas_99silva@hotmail.com>
2025-04-19 23:02:41 -0300

    feat: ensure AccountFirebaseRepository saves an user on success

--- src/infra/repositories/firebase/account-firebase-repository.ts
+++ src/infra/repositories/firebase/account-firebase-repository.ts
@@ -1,12 +1,30 @@
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { addDoc, collection, getFirestore, Timestamp } from 'firebase/firestore'
+
 import { auth } from '@/main/config/firebase'
 import {
   AddAccountRepository,
   AddAccountRepositoryParams,
+  SaveUserRepository,
+  SaveUserRepositoryParams,
 } from '@/data/contracts'
-import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { userConverter } from './converters'
 
-export class AccountFirebaseRepository implements AddAccountRepository {
+export const db = getFirestore()
+
+export class AccountFirebaseRepository
+  implements AddAccountRepository, SaveUserRepository
+{
   async add(account: AddAccountRepositoryParams): Promise<void> {
     await createUserWithEmailAndPassword(auth, account.email, account.password)
+  }
+
+  async save(user: SaveUserRepositoryParams): Promise<void> {
+    await addDoc(collection(db, 'users').withConverter(userConverter), {
+      name: user.name,
+      email: user.email,
+      createdAt: Timestamp.now(),
+      updatedAt: Timestamp.now(),
+    })
   }
 }
---
+++ src/infra/repositories/firebase/converters/User.ts
@@ -0,0 +1,26 @@
+import { SaveUserRepositoryParams } from '@/data/contracts'
+import {
+  DocumentData,
+  FirestoreDataConverter,
+  Timestamp,
+} from 'firebase/firestore'
+
+type User = SaveUserRepositoryParams & {
+  createdAt: Timestamp
+  updatedAt: Timestamp
+}
+
+export const userConverter: FirestoreDataConverter<User> = {
+  toFirestore: (user: User): DocumentData => {
+    return {
+      name: user.name,
+      email: user.email,
+      createdAt: user.createdAt,
+      updatedAt: user.updatedAt,
+    }
+  },
+  fromFirestore: (snapshot, options): User => {
+    const data = snapshot.data(options)
+    return data as User
+  },
+}
