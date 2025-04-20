35695772f1f740ee6855b2c5d6d5fb26e798f097
Lucas Vieira <lucas_99silva@hotmail.com>
2025-04-19 23:39:47 -0300

    feat: add AddAccountImpl composition

--- src/main/factories/usecases/add-account-factory.ts
+++ src/main/factories/usecases/add-account-factory.ts
@@ -3,6 +3,9 @@ import { AddAccount } from '@/domain/usecases'
 import { AccountFirebaseRepository } from '@/infra/repositories/firebase'
 
 export const makeAddAccount = (): AddAccount => {
-  const addAccountRepository = new AccountFirebaseRepository()
-  return new AddAccountImpl(addAccountRepository)
+  const accountFirebaseRepository = new AccountFirebaseRepository()
+  return new AddAccountImpl(
+    accountFirebaseRepository,
+    accountFirebaseRepository,
+  )
 }
