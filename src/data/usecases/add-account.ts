a4704faf955df9eded5b2e74773efe11f751c36b
Lucas Vieira <lucas_99silva@hotmail.com>
2025-04-19 23:14:27 -0300

    feat: ensure AddAccount calls SaveUserRepository with correct values

--- src/data/usecases/add-account.ts
+++ src/data/usecases/add-account.ts
@@ -1,14 +1,23 @@
 import { AddAccount, AddAccountParams } from '@/domain/usecases'
-import { AddAccountRepository } from '@/data/contracts'
+import { AddAccountRepository, SaveUserRepository } from '@/data/contracts'
 
 export class AddAccountImpl implements AddAccount {
   private addAccountRepository
+  private saveUserRepository
 
-  constructor(addAccountRepository: AddAccountRepository) {
+  constructor(
+    addAccountRepository: AddAccountRepository,
+    saveUserRepository: SaveUserRepository,
+  ) {
     this.addAccountRepository = addAccountRepository
+    this.saveUserRepository = saveUserRepository
   }
 
   async execute(account: AddAccountParams): Promise<void> {
     await this.addAccountRepository.add(account)
+    await this.saveUserRepository.save({
+      name: account.name,
+      email: account.email,
+    })
   }
 }
