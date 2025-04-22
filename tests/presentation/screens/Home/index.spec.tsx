e9e9fb51cfc5830503abe0346666b272b197e9c5
Lucas Vieira <lucas_99silva@hotmail.com>
2025-04-22 11:42:10 -0300

    test: ensure Home shows passwordError if field password is empty

--- tests/presentation/screens/Home/index.spec.tsx
+++ tests/presentation/screens/Home/index.spec.tsx
@@ -243,5 +243,16 @@ describe('<Home />', () => {
         expect(errorEmail.props.children).toBe('Endereço de e-mail inválido')
       })
     })
+
+    it('should show passwordError if field password is empty', async () => {
+      makeLoginSut()
+      const submitButton = await screen.findByTestId('submit-button')
+      fireEvent(submitButton, 'press')
+      const errorEmail = await screen.findByTestId('error-password')
+
+      await waitFor(async () => {
+        expect(errorEmail.props.children).toBe('A senha é obrigatória')
+      })
+    })
   })
 })
