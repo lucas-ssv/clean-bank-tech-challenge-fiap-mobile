1eae3d09f1b73775fcafa915e5618d18c585d302
Lucas Vieira <lucas_99silva@hotmail.com>
2025-04-23 14:24:28 -0300

    test: ensure AddTransaction throws if AddTransactionDocumentRepository throws

--- tests/data/usecases/transaction/add-transaction.spec.ts
+++ tests/data/usecases/transaction/add-transaction.spec.ts
@@ -244,4 +244,29 @@ describe('AddTransaction usecase', () => {
 
     expect(promise).rejects.toThrow()
   })
+
+  it('should throw if AddTransactionDocumentRepository throws', async () => {
+    const { sut, addTransactionDocumentRepositoryMock } = makeSut()
+    jest
+      .spyOn(addTransactionDocumentRepositoryMock, 'add')
+      .mockImplementationOnce(() => {
+        throw new Error()
+      })
+
+    const promise = sut.execute({
+      transactionType: TransactionType.CAMBIO_DE_MOEDA,
+      transactionDocuments: [
+        {
+          mimeType: 'any_mimetype',
+          name: 'any_name',
+          uri: 'any_uri',
+        },
+      ],
+      date: new Date(),
+      value: 100,
+      userUID: 'any_user_uid',
+    })
+
+    expect(promise).rejects.toThrow()
+  })
 })
