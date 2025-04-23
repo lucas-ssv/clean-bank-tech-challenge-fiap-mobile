4188081814525812f99d3084b0704803523f139d
Lucas Vieira <lucas_99silva@hotmail.com>
2025-04-23 11:07:37 -0300

    test: ensure AddTransaction calls UploadTransactionDocumentService with correct values

--- tests/data/usecases/transaction/add-transaction.spec.ts
+++ tests/data/usecases/transaction/add-transaction.spec.ts
@@ -9,16 +9,26 @@ jest.useFakeTimers()
 
 class AddTransactionImpl implements AddTransaction {
   private addTransactionRepository
+  private uploadTransactionDocumentService
 
-  constructor(addTransactionRepository: AddTransactionRepository) {
+  constructor(
+    addTransactionRepository: AddTransactionRepository,
+    uploadTransactionDocumentService: UploadTransactionDocumentServiceMock,
+  ) {
     this.addTransactionRepository = addTransactionRepository
+    this.uploadTransactionDocumentService = uploadTransactionDocumentService
   }
 
   async execute(
     transaction: AddTransactionParams,
   ): Promise<AddTransactionModel> {
     await this.addTransactionRepository.add(transaction)
-    return Promise.resolve(null as any)
+
+    for (const transactionDocument of transaction.transactionDocuments) {
+      await this.uploadTransactionDocumentService.upload(transactionDocument)
+    }
+
+    return null as any
   }
 }
 
@@ -47,11 +57,20 @@ class AddTransactionRepositoryStub implements AddTransactionRepository {
   }
 }
 
+class UploadTransactionDocumentServiceMock {
+  async upload(transactionDocument: any): Promise<void> {}
+}
+
 describe('AddTransaction usecase', () => {
   it('should call AddTransactionRepository with correct values', async () => {
     const addTransactionRepositoryStub = new AddTransactionRepositoryStub()
     const addSpy = jest.spyOn(addTransactionRepositoryStub, 'add')
-    const sut = new AddTransactionImpl(addTransactionRepositoryStub)
+    const uploadTransactionDocumentServiceMock =
+      new UploadTransactionDocumentServiceMock()
+    const sut = new AddTransactionImpl(
+      addTransactionRepositoryStub,
+      uploadTransactionDocumentServiceMock,
+    )
 
     await sut.execute({
       transactionType: TransactionType.CAMBIO_DE_MOEDA,
@@ -82,5 +101,39 @@ describe('AddTransaction usecase', () => {
       value: 100,
       userUID: 'any_user_uid',
     })
+  })
+
+  it('should call UploadTransactionDocumentService with correct values', async () => {
+    const addTransactionRepositoryStub = new AddTransactionRepositoryStub()
+    const uploadTransactionDocumentServiceMock =
+      new UploadTransactionDocumentServiceMock()
+    const uploadSpy = jest.spyOn(uploadTransactionDocumentServiceMock, 'upload')
+    const sut = new AddTransactionImpl(
+      addTransactionRepositoryStub,
+      uploadTransactionDocumentServiceMock,
+    )
+
+    await sut.execute({
+      transactionType: TransactionType.CAMBIO_DE_MOEDA,
+      transactionDocuments: [
+        {
+          mimeType: 'any_mimetype',
+          name: 'any_name',
+          transactionId: 'any_transaction_id',
+          uri: 'any_uri',
+        },
+      ],
+      date: new Date(),
+      value: 100,
+      userUID: 'any_user_uid',
+    })
+
+    expect(uploadSpy).toHaveBeenCalledWith({
+      mimeType: 'any_mimetype',
+      name: 'any_name',
+      transactionId: 'any_transaction_id',
+      uri: 'any_uri',
+    })
+    expect(uploadSpy).toHaveBeenCalledTimes(1)
   })
 })
