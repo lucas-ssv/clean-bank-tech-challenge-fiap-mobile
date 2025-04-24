068ca87a86a914364716e994f348aac48748cbf8
Lucas Vieira <lucas_99silva@hotmail.com>
2025-04-24 09:52:55 -0300

    test: ensure UploadFirebaseService calls ref with correct values

--- tests/infra/services/firebase/upload-firebase-service.spec.ts
+++ tests/infra/services/firebase/upload-firebase-service.spec.ts
@@ -1,14 +1,47 @@
 import { randomUUID } from 'node:crypto'
 
 import { UploadTransactionDocumentService } from '@/data/contracts/services'
+import { ref } from 'firebase/storage'
+import { storage } from '@/main/config/firebase'
 
 jest.mock('node:crypto', () => ({
-  randomUUID: jest.fn(),
+  randomUUID: jest.fn().mockReturnValue('any_filename'),
+}))
+
+jest.mock('firebase/auth', () => ({
+  signInWithEmailAndPassword: jest.fn(),
+  createUserWithEmailAndPassword: jest.fn(),
+  initializeAuth: jest.fn(),
+  getReactNativePersistence: jest.fn(),
+  setPersistence: jest.fn(),
+}))
+
+jest.mock('firebase/firestore', () => ({
+  addDoc: jest.fn().mockResolvedValue({ id: 'any_transaction_id' }),
+  collection: jest.fn(),
+  getFirestore: jest.fn(),
+  Timestamp: {
+    now: jest.fn(() => 'any_timestamp'),
+  },
+}))
+
+jest.mock('firebase/app', () => ({
+  initializeApp: jest.fn(),
+}))
+
+jest.mock('firebase/storage', () => ({
+  getStorage: jest.fn(),
+  ref: jest.fn(),
+}))
+
+jest.mock('@/main/config/firebase', () => ({
+  storage: 'mocked_storage',
 }))
 
 class UploadFirebaseService implements UploadTransactionDocumentService {
   async upload(uri: string): Promise<string> {
-    randomUUID()
+    const fileName = randomUUID()
+    ref(storage, `transaction-documents/${fileName}`)
     return ''
   }
 }
@@ -20,5 +53,16 @@ describe('UploadFirebaseService', () => {
     await sut.upload('any_document_uri')
 
     expect(randomUUID).toHaveBeenCalled()
+  })
+
+  it('should call ref with correct values', async () => {
+    const sut = new UploadFirebaseService()
+
+    await sut.upload('any_document_uri')
+
+    expect(ref).toHaveBeenCalledWith(
+      storage,
+      'transaction-documents/any_filename',
+    )
   })
 })
