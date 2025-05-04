45fc218808067b778737a6bd89e572b5ecec2c30
Lucas Vieira <lucas_99silva@hotmail.com>
2025-05-03 23:05:28 -0300

    refactor: move sut creation to a factory method

--- tests/data/usecases/account/observe-auth-state.spec.ts
+++ tests/data/usecases/account/observe-auth-state.spec.ts
@@ -5,8 +5,8 @@ class ObserveAuthStateImpl implements ObserveAuthState {
   private loadAccountByEmailRepository
 
   constructor(
-    authRepository: AuthRepositoryMock,
-    loadAccountByEmailRepository: LoadAccountByEmailRepositoryMock,
+    authRepository: AuthRepository,
+    loadAccountByEmailRepository: LoadAccountByEmailRepository,
   ) {
     this.authRepository = authRepository
     this.loadAccountByEmailRepository = loadAccountByEmailRepository
@@ -20,26 +20,50 @@ class ObserveAuthStateImpl implements ObserveAuthState {
   }
 }
 
-class AuthRepositoryMock {
-  onAuthStateChanged(callback: any): void {
+interface AuthRepository {
+  onAuthStateChanged: (callback: (user: { email: string }) => void) => void
+}
+
+class AuthRepositoryStub implements AuthRepository {
+  onAuthStateChanged(callback: (user: { email: string }) => void): void {
     callback({ email: 'any_email@mail.com' })
   }
 }
 
-class LoadAccountByEmailRepositoryMock {
+interface LoadAccountByEmailRepository {
+  loadByEmail: (email: string) => Promise<void>
+}
+
+class LoadAccountByEmailRepositoryMock implements LoadAccountByEmailRepository {
   async loadByEmail(email: string): Promise<void> {}
+}
+
+type SutTypes = {
+  sut: ObserveAuthStateImpl
+  authRepositoryStub: AuthRepositoryStub
+  loadAccountByEmailRepositoryMock: LoadAccountByEmailRepositoryMock
+}
+
+const makeSut = (): SutTypes => {
+  const authRepositoryStub = new AuthRepositoryStub()
+  const loadAccountByEmailRepositoryMock =
+    new LoadAccountByEmailRepositoryMock()
+  const sut = new ObserveAuthStateImpl(
+    authRepositoryStub,
+    loadAccountByEmailRepositoryMock,
+  )
+
+  return {
+    sut,
+    authRepositoryStub,
+    loadAccountByEmailRepositoryMock,
+  }
 }
 
 describe('ObserveAuthState', () => {
   it('should call AuthRepository with correct values', async () => {
-    const authRepositoryMock = new AuthRepositoryMock()
-    const loadAccountByEmailRepositoryMock =
-      new LoadAccountByEmailRepositoryMock()
-    const authSpy = jest.spyOn(authRepositoryMock, 'onAuthStateChanged')
-    const sut = new ObserveAuthStateImpl(
-      authRepositoryMock,
-      loadAccountByEmailRepositoryMock,
-    )
+    const { sut, authRepositoryStub } = makeSut()
+    const authSpy = jest.spyOn(authRepositoryStub, 'onAuthStateChanged')
 
     sut.execute()
 
@@ -47,14 +71,8 @@ describe('ObserveAuthState', () => {
   })
 
   it('should call LoadAccountByEmailRepository with correct values', async () => {
-    const authRepositoryMock = new AuthRepositoryMock()
-    const loadAccountByEmailRepositoryMock =
-      new LoadAccountByEmailRepositoryMock()
+    const { sut, loadAccountByEmailRepositoryMock } = makeSut()
     const loadSpy = jest.spyOn(loadAccountByEmailRepositoryMock, 'loadByEmail')
-    const sut = new ObserveAuthStateImpl(
-      authRepositoryMock,
-      loadAccountByEmailRepositoryMock,
-    )
 
     sut.execute()
 
