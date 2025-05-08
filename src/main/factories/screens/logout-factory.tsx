934e1bba5473594256e877cbe97b397beb58a4e1
Lucas Vieira <lucas_99silva@hotmail.com>
2025-05-08 14:04:09 -0300

    refactor: move logout to its own file

--- src/main/factories/screens/logout-factory.tsx
+++ src/main/factories/screens/logout-factory.tsx
@@ -1,10 +1,6 @@
-import { LogoutImpl } from '@/data/usecases/account'
-import { AccountFirebaseRepository } from '@/infra/repositories/firebase'
 import { AvatarMenu } from '@/main/routes/app/(stack)/components'
+import { makeLogout } from '@/main/factories/usecases/screens/account'
 
 export function MakeAvatarMenu() {
-  const logoutAccountRepository = new AccountFirebaseRepository()
-  const logout = new LogoutImpl(logoutAccountRepository)
-
-  return <AvatarMenu logout={logout} />
+  return <AvatarMenu logout={makeLogout()} />
 }
