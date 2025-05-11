d26d03c6f59cdd8d5afc4a503e2fe9633c2bf7e2
Lucas Vieira <lucas_99silva@hotmail.com>
2025-05-10 23:32:23 -0300

    feat: ensure TransactionFirebaseRepository loads transactions by date on success

--- src/infra/repositories/firebase/transaction-firebase-repository.ts
+++ src/infra/repositories/firebase/transaction-firebase-repository.ts
@@ -1,13 +1,24 @@
-import { addDoc, collection, Timestamp } from 'firebase/firestore'
+import {
+  addDoc,
+  collection,
+  getDocs,
+  query,
+  Timestamp,
+  where,
+} from 'firebase/firestore'
 
 import {
   AddTransactionRepository,
   AddTransactionRepositoryParams,
+  LoadTransactionsByDateRepository,
+  LoadTransactionsByDateRepositoryResult,
 } from '@/data/contracts/transaction'
-import { db } from '@/main/config/firebase'
+import { auth, db } from '@/main/config/firebase'
 import { transactionConverter } from './converters'
 
-export class TransactionFirebaseRepository implements AddTransactionRepository {
+export class TransactionFirebaseRepository
+  implements AddTransactionRepository, LoadTransactionsByDateRepository
+{
   async add(transaction: AddTransactionRepositoryParams): Promise<string> {
     const transactionRef = await addDoc(
       collection(db, 'transactions').withConverter(transactionConverter),
@@ -21,5 +32,31 @@ export class TransactionFirebaseRepository implements AddTransactionRepository {
       },
     )
     return transactionRef.id
+  }
+
+  async loadByDate(
+    startDate: Date,
+    endDate: Date,
+  ): Promise<LoadTransactionsByDateRepositoryResult[]> {
+    const q = query(
+      collection(db, 'transactions').withConverter(transactionConverter),
+      where('userUID', '==', auth.currentUser?.uid),
+      where('date', '>=', startDate),
+      where('date', '<=', endDate),
+    )
+    const querySnapshot = await getDocs(q)
+    const transactions: LoadTransactionsByDateRepositoryResult[] = []
+    querySnapshot.forEach((doc) => {
+      const transaction = doc.data()
+      transactions.push({
+        date: transaction.date,
+        transactionType: transaction.transactionType,
+        value: transaction.value,
+        userUID: transaction.userUID,
+        createdAt: transaction.createdAt.toDate(),
+        updatedAt: transaction.updatedAt.toDate(),
+      })
+    })
+    return transactions
   }
 }
