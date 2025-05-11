ff24f1e1504a5977028267d57e6f55cbcda9cd7e
Lucas Vieira <lucas_99silva@hotmail.com>
2025-05-10 23:06:38 -0300

    refactor: add a new describe to separate tests

--- tests/infra/repositories/firebase/transaction-firebase-repository.spec.ts
+++ tests/infra/repositories/firebase/transaction-firebase-repository.spec.ts
@@ -31,50 +31,52 @@ jest.mock('firebase/storage', () => ({
 }))
 
 describe('TransactionFirebaseRepository', () => {
-  it('should add a transaction on success', async () => {
-    const mockedCollectionWithConverter = 'mockedCollectionWithConverter'
-    const withConverterMock = jest
-      .fn()
-      .mockReturnValue(mockedCollectionWithConverter)
-    ;(collection as jest.Mock).mockReturnValue({
-      withConverter: withConverterMock,
-    })
-    const sut = new TransactionFirebaseRepository()
+  describe('add()', () => {
+    it('should add a transaction on success', async () => {
+      const mockedCollectionWithConverter = 'mockedCollectionWithConverter'
+      const withConverterMock = jest
+        .fn()
+        .mockReturnValue(mockedCollectionWithConverter)
+      ;(collection as jest.Mock).mockReturnValue({
+        withConverter: withConverterMock,
+      })
+      const sut = new TransactionFirebaseRepository()
 
-    await sut.add({
-      transactionType: TransactionType.CAMBIO_DE_MOEDA,
-      date: new Date(),
-      value: 100,
-      userUID: 'any_user_uid',
-    })
+      await sut.add({
+        transactionType: TransactionType.CAMBIO_DE_MOEDA,
+        date: new Date(),
+        value: 100,
+        userUID: 'any_user_uid',
+      })
 
-    expect(addDoc).toHaveBeenCalledWith(mockedCollectionWithConverter, {
-      transactionType: TransactionType.CAMBIO_DE_MOEDA,
-      date: new Date(),
-      value: 100,
-      userUID: 'any_user_uid',
-      createdAt: 'any_timestamp',
-      updatedAt: 'any_timestamp',
-    })
-  })
-
-  it('should return a transaction id on success', async () => {
-    const mockedCollectionWithConverter = 'mockedCollectionWithConverter'
-    const withConverterMock = jest
-      .fn()
-      .mockReturnValue(mockedCollectionWithConverter)
-    ;(collection as jest.Mock).mockReturnValue({
-      withConverter: withConverterMock,
-    })
-    const sut = new TransactionFirebaseRepository()
-
-    const transactionId = await sut.add({
-      transactionType: TransactionType.CAMBIO_DE_MOEDA,
-      date: new Date(),
-      value: 100,
-      userUID: 'any_user_uid',
+      expect(addDoc).toHaveBeenCalledWith(mockedCollectionWithConverter, {
+        transactionType: TransactionType.CAMBIO_DE_MOEDA,
+        date: new Date(),
+        value: 100,
+        userUID: 'any_user_uid',
+        createdAt: 'any_timestamp',
+        updatedAt: 'any_timestamp',
+      })
     })
 
-    expect(transactionId).toBe('any_transaction_id')
+    it('should return a transaction id on success', async () => {
+      const mockedCollectionWithConverter = 'mockedCollectionWithConverter'
+      const withConverterMock = jest
+        .fn()
+        .mockReturnValue(mockedCollectionWithConverter)
+      ;(collection as jest.Mock).mockReturnValue({
+        withConverter: withConverterMock,
+      })
+      const sut = new TransactionFirebaseRepository()
+
+      const transactionId = await sut.add({
+        transactionType: TransactionType.CAMBIO_DE_MOEDA,
+        date: new Date(),
+        value: 100,
+        userUID: 'any_user_uid',
+      })
+
+      expect(transactionId).toBe('any_transaction_id')
+    })
   })
 })
