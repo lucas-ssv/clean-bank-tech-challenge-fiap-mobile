924476ad07b20ae167610b13fae6169fd88aab71
Lucas Vieira <lucas_99silva@hotmail.com>
2025-05-12 18:04:22 -0300

    feat: ensure TransactionFirebaseRepository load all transactions on success

--- src/infra/repositories/firebase/transaction-firebase-repository.ts
+++ src/infra/repositories/firebase/transaction-firebase-repository.ts
@@ -12,12 +12,17 @@ import {
   AddTransactionRepositoryParams,
   LoadTransactionsByDateRepository,
   LoadTransactionsByDateRepositoryResult,
+  LoadTransactionsRepository,
+  LoadTransactionsRepositoryResult,
 } from '@/data/contracts/transaction'
 import { auth, db } from '@/main/config/firebase'
 import { transactionConverter } from './converters'
 
 export class TransactionFirebaseRepository
-  implements AddTransactionRepository, LoadTransactionsByDateRepository
+  implements
+    AddTransactionRepository,
+    LoadTransactionsRepository,
+    LoadTransactionsByDateRepository
 {
   async add(transaction: AddTransactionRepositoryParams): Promise<string> {
     const transactionRef = await addDoc(
@@ -32,6 +37,31 @@ export class TransactionFirebaseRepository
       },
     )
     return transactionRef.id
+  }
+
+  async loadAll(): Promise<LoadTransactionsRepositoryResult> {
+    const q = query(
+      collection(db, 'transactions').withConverter(transactionConverter),
+      where('userUID', '==', auth.currentUser?.uid),
+    )
+    const querySnapshot = await getDocs(q)
+    const transactionId: string = querySnapshot.docs[0].id
+    const transactions: any[] = []
+    querySnapshot.forEach((doc) => {
+      const transaction = doc.data()
+      transactions.push({
+        date: transaction.date,
+        transactionType: transaction.transactionType,
+        value: transaction.value,
+        userUID: transaction.userUID,
+        createdAt: transaction.createdAt.toDate(),
+        updatedAt: transaction.updatedAt.toDate(),
+      })
+    })
+    return {
+      transactionId,
+      transactions,
+    }
   }
 
   async loadByDate(
