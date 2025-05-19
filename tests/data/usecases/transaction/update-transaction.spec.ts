c44b5d1a0249ea727215c3934e08da5a5e1868ef
Lucas Vieira <lucas_99silva@hotmail.com>
2025-05-19 11:04:30 -0300

    test: ensure UpdateTransaction calls UpdateTransactionRepository with correct values

---
+++ tests/data/usecases/transaction/update-transaction.spec.ts
@@ -0,0 +1,50 @@
+import { TransactionType } from '@/domain/models/transaction'
+import {
+  UpdateTransaction,
+  UpdateTransactionData,
+} from '@/domain/usecases/transaction'
+
+jest.useFakeTimers()
+
+class UpdateTransactionImpl implements UpdateTransaction {
+  private updateTransactionRepository: UpdateTransactionRepositoryMock
+
+  constructor(updateTransactionRepository: UpdateTransactionRepositoryMock) {
+    this.updateTransactionRepository = updateTransactionRepository
+  }
+
+  async execute(
+    transactionId: string,
+    transactionData: UpdateTransactionData,
+  ): Promise<void> {
+    await this.updateTransactionRepository.update(
+      transactionId,
+      transactionData,
+    )
+  }
+}
+
+class UpdateTransactionRepositoryMock {
+  async update(transactionId: string, transactionData: any): Promise<void> {}
+}
+
+describe('UpdateTransaction usecase', () => {
+  it('should call UpdateTransactionRepository with correct values', async () => {
+    const updateTransactionRepositoryMock =
+      new UpdateTransactionRepositoryMock()
+    const updateSpy = jest.spyOn(updateTransactionRepositoryMock, 'update')
+    const sut = new UpdateTransactionImpl(updateTransactionRepositoryMock)
+
+    await sut.execute('any_transaction_id', {
+      transactionType: TransactionType.CAMBIO_DE_MOEDA,
+      value: 100,
+      date: new Date(),
+    })
+
+    expect(updateSpy).toHaveBeenCalledWith('any_transaction_id', {
+      transactionType: TransactionType.CAMBIO_DE_MOEDA,
+      value: 100,
+      date: new Date(),
+    })
+  })
+})
